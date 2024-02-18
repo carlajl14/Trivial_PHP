9953ed42ae28cca3420cfdb9ec3c9721e427e953
carla <carlajilo@gmail.com>
2024-02-18 11:37:55 +0100

    response

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
                     <label for="amount">Número de preguntas:</label>
                     <input type="number" name="amount" class="form-control" min="1" max="50">
                 </div>
-                <div class="form-group">
+                <div class="form-group mt-2">
                     <label for="category">Categoría</label>
                     <select class="form-control" name="category">
                         <option value="">Elige una categoría</option>
@@ -24,13 +24,13 @@
                         $categorie = json_decode($categories, true);
                         if (isset($categorie)) {
                             foreach ($categorie['trivia_categories'] as $category) {
-                                echo '<option value="' . $category['name'] . '">' . $category['name'] . '</option>';
+                                echo '<option value="' . $category['id'] . '">' . $category['name'] . '</option>';
                             }
                         }
                         ?>
                     </select>
                 </div>
-                <div class="form-group">
+                <div class="form-group mt-2">
                     <label for="category">Dificultad</label>
                     <select class="form-control" name="difficulty">
                         <option value="">Elige la dificultad</option>
@@ -42,8 +42,36 @@
                         ?>
                     </select>
                 </div>
-                <input type="submit" name="start" value="Comenzar" class="btn btn-primary">
+                <input type="submit" name="start" value="Comenzar" class="btn btn-primary mt-2">
             </form>
+            <?php
+            //Comprobar si los parámetros se han elegido
+            if (isset($_POST['start']) && isset($_POST['amount']) && isset($_POST['category'])) {
+                $amount = $_POST['amount'];
+                $category = $_POST['category'];
+                $difficulty = $_POST['difficulty'];
+
+                //Obtener los datos de la API
+                $questions = file_get_contents("https://opentdb.com/api.php?amount=" . $amount . "&category=" . $category . "&difficulty=" . $difficulty);
+                $dataQuestions = json_decode($questions, true);
+                $cont = 0;
+                
+                //Recorrer el array de datos
+                foreach ($dataQuestions['results'] as $data) {
+                    echo '<h2>Preguntas</h2>';
+                    $cont++;
+                    echo '<h3>Pregunta ' . $cont . '</h3>';
+                    echo $data['question'] . '<br>';
+                    echo '<ul>';
+                    echo '<li>' . $data['correct_answer'] . '</li>';
+                    //Recorrer el subarray
+                    foreach($data['incorrect_answers'] as $incorrect) {
+                        echo '<li>' . $incorrect . '</li>';
+                    }
+                    echo '</ul>';
+                }
+            }
+            ?>
         </div>
     </body>
 </html>
